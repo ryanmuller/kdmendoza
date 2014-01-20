cb7753f65ebf5e6289a9886df7c9d404855ce0c2
cicatriz <ryan@geknowm.com>
2014-01-20 13:22:05 -0600

    current version

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*
+window.onscroll = function(e) {
+  h = document.getElementById('header');
+  c = document.getElementById('project-content');
+  if (document.body.scrollTop > h.offsetTop) {
+    if (document.body.scrollTop > c.offsetTop) {
+      h.className = 'sticky';
+    } else {
+      h.className = 'sticky in-header';
+    }
+  }
+
+  if (document.body.scrollTop < 240) {
+    h.className = '';
+  }
+}
+*/
