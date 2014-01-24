3e8283923b6ab6c63d4233b2a784928547ab7969
cicatriz <ryan@geknowm.com>
2014-01-23 21:30:27 -0600

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,13 @@ window.onscroll = function(e) {
   }
 }
 
-$(document).foundation("magellan", {threshold: 65});
+$(document).foundation("magellan", {threshold: 150});
 
 
 $(document).ready(function() {
   $('.magellan a').on('click', function(){
       var elementID = $(this).attr('href').substring(1);
-      var pos = $("[data-magellan-destination="+elementID+"]").offset().top - 65;
+      var pos = $("[data-magellan-destination="+elementID+"]").offset().top - 100;
       console.log("scrolling to",pos);
       $.scrollTo({ endY: pos });
       return false;
